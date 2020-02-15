0a93e1e1d6b86e8c3a5f31e43441f9752fb61078
wincent <wincent.holm@gmail.com>
2020-02-15 15:45:52 +0100

    Proxy items now work properly.
    Started working on triggers.

--- InfectedRose.Triggers/Trigger.cs
+++ InfectedRose.Triggers/Trigger.cs
@@ -1,13 +1,16 @@
+using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace InfectedRose.Triggers
 {
     public class Trigger
     {
+        public int FileId { get; set; }
+        
         [XmlAttribute("id")] public int Id { get; set; }
         
         [XmlAttribute("enabled")] public int Enabled { get; set; }
         
-        [XmlElement("event")] public TriggerEvent[] Events { get; set; }
+        [XmlElement("event")] public List<TriggerEvent> Events { get; set; }
     }
 }
